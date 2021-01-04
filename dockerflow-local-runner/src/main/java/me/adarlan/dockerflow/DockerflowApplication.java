8efb4ec87f515fa18b206ee96aa5916e912bb82b
Adarlan Teixeira <adarlan18@gmail.com>
2021-01-04 16:18:57 -0300

    Change log level to DEBUG

--- dockerflow-local-runner/src/main/java/me/adarlan/dockerflow/DockerflowApplication.java
+++ dockerflow-local-runner/src/main/java/me/adarlan/dockerflow/DockerflowApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DockerflowApplication {
 
 	public static void main(String[] args) {
-		Logger.setLevel(Logger.Level.FOLLOW);
+		Logger.setLevel(Logger.Level.DEBUG);
 		SpringApplication.run(DockerflowApplication.class, args);
 	}
 }
