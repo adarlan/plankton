da7641459e429d2160f79f05db915a195f14b00f
Adarlan Teixeira <adarlan18@gmail.com>
2021-01-05 14:46:50 -0300

    Frontend is in root path and API is in /api

--- local-runner/src/main/java/me/adarlan/dockerflow/DockerflowController.java
+++ local-runner/src/main/java/me/adarlan/dockerflow/DockerflowController.java
@@ -6,10 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @CrossOrigin(origins = "*")
+@RequestMapping("/api")
 public class DockerflowController {
 
     @Autowired
