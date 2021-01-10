378714ee7df327804085aada50a1186a485a9c41
Adarlan Teixeira <adarlan18@gmail.com>
2021-01-10 11:50:53 -0300

    Adapt to changes in config interfaces

--- local-runner/src/main/java/me/adarlan/plankton/PlanktonBeans.java
+++ local-runner/src/main/java/me/adarlan/plankton/PlanktonBeans.java
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.Configuration;
 
 import me.adarlan.plankton.docker.DockerPipelineConfig;
 import me.adarlan.plankton.docker.DockerPipelineFactory;
-import me.adarlan.plankton.serializable.PlanktonSerializer;
 import me.adarlan.plankton.api.Pipeline;
 
 @Configuration
@@ -21,31 +20,51 @@ public class PlanktonBeans {
     @Value("${plankton.workspace}")
     private String workspace;
 
-    @Value("${plankton.docker.metadata}")
-    private String dockerMetadata;
+    @Value("${plankton.metadata}")
+    private String metadata;
 
     @Value("${plankton.docker.host}")
     private String dockerHost;
 
     @Bean
-    public DockerPipelineConfig config() {
-        DockerPipelineConfig config = new DockerPipelineConfig();
-        config.setPipelineId(pipelineId);
-        config.setComposeFile(composeFile);
-        config.setWorkspace(workspace);
-        config.setMetadata(dockerMetadata);
-        config.setDockerHost(dockerHost);
-        return config;
+    public DockerPipelineConfig dockerPipelineConfig() {
+        return new DockerPipelineConfig() {
+
+            @Override
+            public String getPipelineId() {
+                return String.valueOf(this.hashCode());
+            }
+
+            @Override
+            public String getComposeFilePath() {
+                return composeFile;
+            }
+
+            @Override
+            public String getWorkspaceDirectoryPath() {
+                return workspace;
+            }
+
+            @Override
+            public String getMetadataDirectoryPath() {
+                return metadata;
+            }
+
+            @Override
+            public String getDockerHost() {
+                return dockerHost;
+            }
+        };
     }
 
     @Bean
     public Pipeline pipeline() {
         DockerPipelineFactory pipelineFactory = new DockerPipelineFactory();
-        return pipelineFactory.createPipeline(config());
+        return pipelineFactory.createPipeline(dockerPipelineConfig());
     }
 
-    @Bean
-    PlanktonSerializer planktonSerializer() {
-        return new PlanktonSerializer(pipeline());
-    }
+    // @Bean
+    // PlanktonSerializer planktonSerializer() {
+    // return new PlanktonSerializer(pipeline());
+    // }
 }
