fc1b76fbaff4b50485b6aa1e20ede5639ba5deb2
Adarlan Teixeira <adarlan18@gmail.com>
2021-01-12 16:11:15 -0300

    Update log message and remove unused constructor

--- engine/src/main/java/me/adarlan/plankton/docker/PlanktonDockerException.java
+++ engine/src/main/java/me/adarlan/plankton/docker/PlanktonDockerException.java
@@ -6,11 +6,6 @@ public class PlanktonDockerException extends RuntimeException {
 
     private static final long serialVersionUID = 1L;
 
-    public PlanktonDockerException(Throwable e) {
-        super(e);
-        Logger.getLogger().fatal(() -> e.getClass().getName() + " -> " + e.getMessage());
-    }
-
     public PlanktonDockerException(String msg) {
         super(msg);
         Logger.getLogger().fatal(() -> msg);
@@ -18,6 +13,6 @@ public class PlanktonDockerException extends RuntimeException {
 
     public PlanktonDockerException(String msg, Throwable e) {
         super(msg, e);
-        Logger.getLogger().fatal(() -> msg + ": " + e.getClass().getName() + " -> " + e.getMessage());
+        Logger.getLogger().fatal(() -> msg + "; " + e.getClass().getSimpleName() + ": " + e.getMessage());
     }
 }
