aecdd2154b17af436bbbea4be9b660984770bcb7
Adarlan Teixeira <adarlan18@gmail.com>
2021-01-13 19:37:18 -0300

    Update DockerPipelineFactory

--- engine/src/main/java/me/adarlan/plankton/docker/DockerPipelineFactory.java
+++ engine/src/main/java/me/adarlan/plankton/docker/DockerPipelineFactory.java
@@ -5,9 +5,39 @@ import me.adarlan.plankton.core.PipelineFactory;
 public class DockerPipelineFactory extends PipelineFactory<PipelineImplementation, DockerPipelineConfig> {
 
     @Override
-    public PipelineImplementation createPipeline(DockerPipelineConfig config) {
-        DockerCompose dockerCompose = new DockerCompose(config);
-        PipelineImplementation pipeline = new PipelineImplementation(dockerCompose);
-        return pipeline;
+    public PipelineImplementation createPipeline(DockerPipelineConfig pipelineConfiguration) {
+        DockerComposeConfiguration composeConfiguration = composeConfiguration(pipelineConfiguration);
+        DockerCompose dockerCompose = new DockerCompose(composeConfiguration);
+        return new PipelineImplementation(dockerCompose);
+    }
+
+    private DockerComposeConfiguration composeConfiguration(DockerPipelineConfig pipelineConfig) {
+        return new DockerComposeConfiguration() {
+
+            @Override
+            public String projectName() {
+                return pipelineConfig.getPipelineId();
+            }
+
+            @Override
+            public String filePath() {
+                return pipelineConfig.getComposeFilePath();
+            }
+
+            @Override
+            public String projectDirectory() {
+                return pipelineConfig.getWorkspaceDirectoryPath();
+            }
+
+            @Override
+            public String metadataDirectory() {
+                return pipelineConfig.getMetadataDirectoryPath() + "/" + pipelineConfig.getPipelineId();
+            }
+
+            @Override
+            public String dockerHost() {
+                return pipelineConfig.getDockerHost();
+            }
+        };
     }
 }
