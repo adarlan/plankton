ab5ba7129383e139ff166b8c7d448ee2bd144973
Adarlan Teixeira <adarlan18@gmail.com>
2021-01-14 16:10:37 -0300

    Add TODO

--- engine/src/main/java/me/adarlan/plankton/core/ServiceDependencyStatus.java
+++ engine/src/main/java/me/adarlan/plankton/core/ServiceDependencyStatus.java
@@ -2,4 +2,6 @@ package me.adarlan.plankton.core;
 
 public enum ServiceDependencyStatus {
     WAITING, PASSED, BLOCKED;
+    // TODO remove this enum
+    // the status should be checked on each pipeline iteration
 }
