d6814013c6cc73bfb4e325a9533ece56e81c48e3
Adarlan Teixeira <adarlan18@gmail.com>
2021-01-15 20:13:36 -0300

    Web server is optional

--- local-runner/src/main/java/me/adarlan/plankton/PlanktonController.java
+++ local-runner/src/main/java/me/adarlan/plankton/PlanktonController.java
@@ -1,6 +1,7 @@
 package me.adarlan.plankton;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import me.adarlan.plankton.workflow.Pipeline;
 
 @RestController
+// @ConditionalOnProperty("plankton.web.enable")
 @CrossOrigin(origins = "*")
 @RequestMapping("/api")
 public class PlanktonController {
