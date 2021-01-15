accc8e3d346e4be3a1d5c221773b0bdbd3c31c89
Adarlan Teixeira <adarlan18@gmail.com>
2021-01-15 08:35:46 -0300

    Remove PipelineConfig and PipelineFactory

--- engine/src/main/java/me/adarlan/plankton/core/PipelineFactory.java
+++
@@ -1,6 +0,0 @@
-package me.adarlan.plankton.core;
-
-public abstract class PipelineFactory<P extends Pipeline, C extends PipelineConfig> {
-
-    public abstract P createPipeline(C config);
-}
--- engine/src/main/java/me/adarlan/plankton/docker/DockerPipelineConfig.java
+++
@@ -1,8 +0,0 @@
-package me.adarlan.plankton.docker;
-
-import me.adarlan.plankton.core.PipelineConfig;
-
-public interface DockerPipelineConfig extends PipelineConfig {
-
-    String getDockerHost();
-}
