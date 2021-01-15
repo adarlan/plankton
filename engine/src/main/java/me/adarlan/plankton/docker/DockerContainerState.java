5f9fd5d20a0064eed580baf03c918d90fa410f99
Adarlan Teixeira <adarlan18@gmail.com>
2021-01-15 01:40:01 -0300

    DockerContainerState implements ContainerState

--- engine/src/main/java/me/adarlan/plankton/docker/DockerContainerState.java
+++ engine/src/main/java/me/adarlan/plankton/docker/DockerContainerState.java
@@ -1,11 +1,14 @@
 package me.adarlan.plankton.docker;
 
+import java.time.Instant;
+
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-import lombok.Data;
+import lombok.NoArgsConstructor;
+import me.adarlan.plankton.compose.ContainerState;
 
-@Data
-public class DockerContainerState {
+@NoArgsConstructor
+public class DockerContainerState implements ContainerState {
 
     @JsonProperty("Status")
     String status;
@@ -39,4 +42,53 @@ public class DockerContainerState {
 
     @JsonProperty("FinishedAt")
     String finishedAt;
+
+    @Override
+    public boolean running() {
+        return status.equals("running");
+    }
+
+    @Override
+    public boolean exited() {
+        return status.equals("exited");
+    }
+
+    @Override
+    public Instant initialInstant() {
+        return parseInstant(startedAt);
+    }
+
+    @Override
+    public Instant finalInstant() {
+        return parseInstant(finishedAt);
+    }
+
+    @Override
+    public Integer exitCode() {
+        return exitCode;
+    }
+
+    private Instant parseInstant(String text) {
+        if (text.equals("0001-01-01T00:00:00Z"))
+            return null;
+        else
+            return Instant.parse(text);
+    }
+
+            // } else if (!containerState.error.isBlank()) {
+        //     running = false;
+        //     ended = true;
+        //     if (initialInstant == null) {
+        //         initialInstant = parseInstant(containerState.startedAt);
+        //     }
+        //     finalInstant = parseInstant(containerState.finishedAt);
+        //     if (containerState.exitCode() != 0) {
+        //         exitCode = containerState.exitCode();
+        //     }
+        // } else if (containerState.status.equals("created")) {
+        //     /* ok */
+        // } else {
+        //     throw new PlanktonDockerException("Unexpected container state: " + containerState);
+        //     // TODO stop thread? failed?
+
 }
