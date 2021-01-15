0cc8a8ae715eeee3f66486d419d8784ed4a17a83
Adarlan Teixeira <adarlan18@gmail.com>
2021-01-15 00:31:47 -0300

    Add boolean methods

--- engine/src/main/java/me/adarlan/plankton/core/ServiceStatus.java
+++ engine/src/main/java/me/adarlan/plankton/core/ServiceStatus.java
@@ -2,4 +2,28 @@ package me.adarlan.plankton.core;
 
 public enum ServiceStatus {
     DISABLED, WAITING, BLOCKED, RUNNING, FAILURE, SUCCESS;
+
+    public boolean isDisabled() {
+        return this == DISABLED;
+    }
+
+    public boolean isWaiting() {
+        return this == WAITING;
+    }
+
+    public boolean isBlocked() {
+        return this == BLOCKED;
+    }
+
+    public boolean isRunning() {
+        return this == RUNNING;
+    }
+
+    public boolean isFailure() {
+        return this == FAILURE;
+    }
+
+    public boolean isSuccess() {
+        return this == SUCCESS;
+    }
 }
