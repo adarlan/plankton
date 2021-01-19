4b42cf243acaa81e6f8aa1d6bfd9d9b8512282d2
Adarlan Teixeira <adarlan18@gmail.com>
2021-01-19 09:19:03 -0300

    Remove unnused import

--- src/main/java/me/adarlan/plankton/spring/PlanktonController.java
+++ src/main/java/me/adarlan/plankton/spring/PlanktonController.java
@@ -1,7 +1,6 @@
 package me.adarlan.plankton.spring;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
