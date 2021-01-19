91ccde063984fd128a557c78efaa850b441b9811
Adarlan Teixeira <adarlan18@gmail.com>
2021-01-18 22:36:05 -0300

    Remove unnused import

--- src/main/java/me/adarlan/plankton/spring/SinglePipelineProvider.java
+++ src/main/java/me/adarlan/plankton/spring/SinglePipelineProvider.java
@@ -1,7 +1,6 @@
 package me.adarlan.plankton.spring;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
