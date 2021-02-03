e24333db3d50cfa99dda6b989c0233b895ca7f18
Adarlan Teixeira <adarlan18@gmail.com>
2021-02-03 12:45:17 -0300

    Rename depends_on contitions EXIT_ZERO -> SUCCEEDED EXIT_NON_ZERO -> FAILED

--- src/main/java/me/adarlan/plankton/compose/DependsOnCondition.java
+++ src/main/java/me/adarlan/plankton/compose/DependsOnCondition.java
@@ -1,7 +1,7 @@
 package me.adarlan.plankton.compose;
 
 public enum DependsOnCondition {
-    SERVICE_HEALTHY, SERVICE_STARTED, EXIT_ZERO, EXIT_NON_ZERO;
+    SERVICE_HEALTHY, SERVICE_STARTED, SUCCEEDED, FAILED;
 
     static DependsOnCondition of(String string) {
         switch (string) {
@@ -9,10 +9,10 @@ public enum DependsOnCondition {
                 return SERVICE_HEALTHY;
             case "service_started":
                 return SERVICE_STARTED;
-            case "exit_zero":
-                return EXIT_ZERO;
-            case "exit_non_zero":
-                return EXIT_NON_ZERO;
+            case "succeeded":
+                return SUCCEEDED;
+            case "failed":
+                return FAILED;
             default:
                 throw new ComposeFileFormatException(
                         "Unexpected " + DependsOnCondition.class.getSimpleName() + ": " + string);
