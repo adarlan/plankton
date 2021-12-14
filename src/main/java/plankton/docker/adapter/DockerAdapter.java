9fcd61f6a9270c91ace75f40fe2015f1be5d9154
Adarlan Teixeira <adarlan18@gmail.com>
2021-12-14 12:32:23 -0300

    Remove unused imports

--- src/main/java/plankton/docker/adapter/DockerAdapter.java
+++ src/main/java/plankton/docker/adapter/DockerAdapter.java
@@ -1,10 +1,8 @@
 package plankton.docker.adapter;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
